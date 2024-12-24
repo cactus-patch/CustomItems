8e04ff19b479e25607cba0bdddebb32de51cfd07
nora <nora@norain.icu>
2024-12-23 18:21:56 -0800

    finish coin custom item

---
+++ Items/Coin.cs
@@ -0,0 +1,59 @@
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Spawn;
+using Exiled.CustomModules.API.Features.Attributes;
+using Exiled.CustomModules.API.Features.CustomItems;
+using Exiled.CustomModules.API.Features.CustomItems.Items;
+using Exiled.Events.EventArgs.Player;
+using PlayerEvents = Exiled.Events.Handlers.Player;
+using MEC;
+
+namespace CustomItems.Items;
+
+[ModuleIdentifier]
+public class Coin {
+  public class Item : CustomItem<Behaviour> {
+    public override string Name => "Coin";
+    public override string Description => "\"What's the most you ever lost on a coin toss?\"";
+
+    public override SettingsBase Settings => new Settings {
+      PickedUpText = new TextDisplay($"You have picked up a <i>{Name}</i>.<br><i>{Description}</i>", channel: TextChannelType.Hint),
+      SelectedText = new TextDisplay($"You have selected a <i>{Name}</i>.<br><i>{Description}</i>", channel: TextChannelType.Hint),
+      NotifyItemToSpectators = true,
+      SpawnProperties = new SpawnProperties {
+        Limit = 3,
+        DynamicSpawnPoints = [
+          new DynamicSpawnPoint { Location = SpawnLocationType.InsideLczCafe, Chance = 0.5f },
+          new DynamicSpawnPoint { Location = SpawnLocationType.InsideLocker, Chance = 0.3f },
+          new DynamicSpawnPoint { Location = SpawnLocationType.InsideServersBottom, Chance = 0.3f },
+          new DynamicSpawnPoint { Location = SpawnLocationType.InsideEscapeSecondary, Chance = 0.5f }
+        ]
+      }
+    };
+  }
+  
+  public class Behaviour : ItemBehaviour {
+    protected override void SubscribeEvents() {
+      PlayerEvents.FlippingCoin += OnFlippingCoin;
+      
+      base.SubscribeEvents();
+    }
+    
+    private void OnFlippingCoin(FlippingCoinEventArgs ev) {
+      if (!Check(ev.Item)) return;
+      Timing.CallDelayed(2f, () => {
+        if (ev.IsTails && !ev.Player.IsDead) {
+          ev.Player.Explode();
+          ev.Player.Kill("You lost.");
+          return;
+        }
+
+        ev.Player.ShowHint("You won -- you feel the adrenaline rushing in your veins.");
+        ev.Player.EnableEffect(EffectType.DamageReduction, intensity: 125, addDurationIfActive: true);
+        ev.Player.EnableEffect(EffectType.RainbowTaste, intensity: 3, addDurationIfActive: true);
+        ev.Player.EnableEffect(EffectType.MovementBoost, intensity: 3, addDurationIfActive: true);
+        ev.Player.EnableEffect(EffectType.Invigorated, addDurationIfActive: true);
+      });
+    }
+  }
+}
--- Items/ExampleItem.cs
+++
@@ -1,7 +0,0 @@
-using Exiled.CustomModules.API.Features.CustomItems;
-
-namespace CustomItems.Items;
-
-public class ExampleItem : CustomItem {
-  public override Type BehaviourComponent => typeof(ExampleItem);
-}
