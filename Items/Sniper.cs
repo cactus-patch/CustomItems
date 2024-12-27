9c87d257ff61faf2832b2e20b3f29c94840016d5
nora <nora@norain.icu>
2024-12-26 18:36:04 -0800

    fix code xd

--- Items/Sniper.cs
+++ Items/Sniper.cs
@@ -4,35 +4,35 @@ using Exiled.API.Features.Spawn;
 using Exiled.CustomModules.API.Features.Attributes;
 using Exiled.CustomModules.API.Features.CustomItems;
 using Exiled.CustomModules.API.Features.CustomItems.Items.Firearms;
-using Exiled.CustomModules.API.Features.Generic;
-using Exiled.Events.EventArgs.Player;
-using MEC;
 
 namespace CustomItems.Items;
 
-public class Sniper{
-    [ModuleIdentifier]
-    public class Item : CustomItem<Behaviour>{
-        public override string Name => "SR-119";
-        public override uint Id {get; set;} = 1291;
-        public override float Weight {get; set;} = 4f;
-        public override bool IsEnabled {get; set;} = true;
-        public override string Description {get; set;} = "A modified E-11 that fires 5.56 at supersonic velocity that deals significantly more damage";
-        public override float Damage {get; set;} = 7.5f;
-        public override byte ClipSize {get; set;} = 1;
-        public override ItemType ItemType => ItemType.GunE11SR;
+public class Sniper {
+  [ModuleIdentifier]
+  public class Item : CustomItem<FirearmBehaviour> {
+    public override string Name => "SR-119";
+    public override uint Id { get; set; } = 1291;
+    public override bool IsEnabled { get; set; } = true;
 
-        public override SettingsBase Settings => new FirearmSettings{
-            PickedUpText = new TextDisplay($"You picked up <i>{Name}</i>,<br><i>{Description}</i>", channel: TextChannelType.Hint),
-            SelectedText = new TextDisplay($"You have selected a <i>{Name}</i>.<br><i>{Description}</i>", channel: TextChannelType.Hint),
-            NotifyItemToSpectators = true,
-            SpawnProperties = new SpawnProperties {
-            Limit = 1,
-            DynamicSpawnPoints = [
-                new DynamicSpawnPoint { Location = SpawnLocationType.InsideHczArmory, Chance = 1f }
-            ]
-            }
-        };
-    }
+    public override string Description { get; set; } =
+      "A modified E-11 that fires 5.56 at supersonic velocity that deals significantly more damage";
 
+    public override ItemType ItemType => ItemType.GunE11SR;
+
+    public override SettingsBase Settings => new FirearmSettings {
+      PickedUpText = new TextDisplay($"You picked up <i>{Name}</i>,<br><i>{Description}</i>",
+        channel: TextChannelType.Hint),
+      SelectedText = new TextDisplay($"You have selected a <i>{Name}</i>.<br><i>{Description}</i>",
+        channel: TextChannelType.Hint),
+      ClipSize = 1,
+      Damage = 7.5f,
+      SpawnProperties = new SpawnProperties() {
+        Limit = 1,
+        DynamicSpawnPoints = [
+          new DynamicSpawnPoint { Location = SpawnLocationType.InsideHczArmory, Chance = 1f }
+        ]
+      },
+      Weight = 8f
+    };
+  }
 }
