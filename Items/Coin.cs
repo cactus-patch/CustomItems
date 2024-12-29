2068ed03eb9d31d3ca9f671b9da3b1ff92ad601f
nora <nora@norain.icu>
2024-12-28 20:45:01 -0800

    reformat coin and change default spawn in escape

--- Items/Coin.cs
+++ Items/Coin.cs
@@ -11,57 +11,55 @@ namespace CustomItems.Items;
 
 [CustomItem(ItemType.Coin)]
 public class Coin : CustomItem {
-    public override string Name { get; set; } = "Coin";
-    public override uint Id { get; set; } = 393;
-    public override string Description { get; set; } = "<i>\"What's the most you ever lost on a coin toss?\"</i>";
-    public override float Weight { get; set; } = 1f;
-    
-    public override SpawnProperties? SpawnProperties { get; set; } = new() {
-      Limit = 3,
-      DynamicSpawnPoints = [
-        new DynamicSpawnPoint { Location = SpawnLocationType.InsideLczCafe, Chance = 50 },
-        new DynamicSpawnPoint { Location = SpawnLocationType.InsideLczWc, Chance = 50 },
-        new DynamicSpawnPoint { Location = SpawnLocationType.InsideHczArmory, Chance = 50 },
-        new DynamicSpawnPoint { Location = SpawnLocationType.InsideEscapeSecondary, Chance = 50 }
-      ]
-    };
+  public override string Name { get; set; } = "Coin";
+  public override uint Id { get; set; } = 393;
+  public override string Description { get; set; } = "<i>\"What's the most you ever lost on a coin toss?\"</i>";
+  public override float Weight { get; set; } = 1f;
 
-    public record Effect(EffectType Type, float Duration, byte Intensity);
+  public override SpawnProperties? SpawnProperties { get; set; } = new() {
+    Limit = 3,
+    DynamicSpawnPoints = [
+      new DynamicSpawnPoint { Location = SpawnLocationType.InsideLczCafe, Chance = 50 },
+      new DynamicSpawnPoint { Location = SpawnLocationType.InsideLczWc, Chance = 50 },
+      new DynamicSpawnPoint { Location = SpawnLocationType.InsideHczArmory, Chance = 50 },
+      new DynamicSpawnPoint { Location = SpawnLocationType.InsideEscapePrimary, Chance = 50 }
+    ]
+  };
 
-    [Description("Effects to give if coin landed on heads.")]
-    public Effect[] Effects { get; set; } = [
-      new (EffectType.DamageReduction, 30, 125),
-      new (EffectType.RainbowTaste, 30, byte.MaxValue),
-      new (EffectType.Invigorated, 30, byte.MaxValue),
-      new (EffectType.MovementBoost, 30, 125)
-    ];
-  
-    protected override void SubscribeEvents() {
-      PlayerEvents.FlippingCoin += OnFlippingCoin;
-      
-      base.SubscribeEvents();
-    }
+  public record Effect(EffectType Type, float Duration, byte Intensity);
 
-    protected override void UnsubscribeEvents() {
-      PlayerEvents.FlippingCoin -= OnFlippingCoin;
-      
-      base.UnsubscribeEvents();
-    }
+  [Description("Effects to give if coin landed on heads.")]
+  public Effect[] Effects { get; set; } = [
+    new(EffectType.DamageReduction, 30, 125),
+    new(EffectType.RainbowTaste, 30, byte.MaxValue),
+    new(EffectType.Invigorated, 30, byte.MaxValue),
+    new(EffectType.MovementBoost, 30, 125)
+  ];
 
-    private void OnFlippingCoin(FlippingCoinEventArgs ev) {
-      if (!Check(ev.Item)) return;
-      Timing.CallDelayed(2f, () => {
-        if (ev.IsTails && !ev.Player.IsDead) {
-          ev.Player.IsGodModeEnabled = false;
-          ev.Player.Explode();
-          ev.Player.Kill("You lost.");
-          return;
-        }
+  protected override void SubscribeEvents() {
+    PlayerEvents.FlippingCoin += OnFlippingCoin;
 
-        ev.Player.ShowHint("You won -- you feel the adrenaline rushing in your veins.");
-        Effects.ForEach((effect) => {
-          ev.Player.EnableEffect(effect.Type, effect.Intensity, effect.Duration, addDurationIfActive: true);
-        });
-      });
-    }
+    base.SubscribeEvents();
+  }
+
+  protected override void UnsubscribeEvents() {
+    PlayerEvents.FlippingCoin -= OnFlippingCoin;
+
+    base.UnsubscribeEvents();
+  }
+
+  private void OnFlippingCoin(FlippingCoinEventArgs ev) {
+    if (!Check(ev.Item)) return;
+    Timing.CallDelayed(2f, () => {
+      if (ev.IsTails && !ev.Player.IsDead) {
+        ev.Player.IsGodModeEnabled = false;
+        ev.Player.Explode();
+        ev.Player.Kill("You lost.");
+        return;
+      }
+
+      ev.Player.ShowHint("You won -- you feel the adrenaline rushing in your veins.");
+      Effects.ForEach((effect) => { ev.Player.EnableEffect(effect.Type, effect.Intensity, effect.Duration, true); });
+    });
+  }
 }
