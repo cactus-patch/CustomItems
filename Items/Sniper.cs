003983b98bbd1eb0d5361133ce8616f45b815d67
nora <nora@norain.icu>
2024-12-28 20:45:41 -0800

    reformat

--- Items/Sniper.cs
+++ Items/Sniper.cs
@@ -9,15 +9,17 @@ namespace CustomItems.Items;
 public class Sniper : CustomWeapon {
   public override uint Id { get; set; } = 1291;
   public override string Name { get; set; } = "SR-119";
+
   public override string Description { get; set; } = "A modified E-11 that fires 5.56 at supersonic velocity that deals significantly more damage";
+
   public override float Weight { get; set; } = 8f;
   public override float Damage { get; set; } = 7.5f;
   public override byte ClipSize { get; set; } = 1;
-  
+
   public override SpawnProperties? SpawnProperties { get; set; } = new() {
     Limit = 1,
     DynamicSpawnPoints = [
-      new DynamicSpawnPoint { Location = SpawnLocationType.InsideHczArmory}
+      new DynamicSpawnPoint { Location = SpawnLocationType.InsideHczArmory }
     ]
   };
 }
--- Items/Tranquilizer.cs
+++ Items/Tranquilizer.cs
@@ -14,50 +14,53 @@ namespace CustomItems.Items;
 
 [CustomItem(ItemType.GunCOM15)]
 public class Tranquilizer : CustomWeapon {
-    public override string Name { get; set; } = "Tranquilizer";
-    public override uint Id { get; set; } = 1290;
-    public override string Description { get; set; } = "A gun that temporarily tranquilizes entities; might be unreliable.";
-    public override float Weight { get; set; } = 1f;
-    public override float Damage { get; set; } = 1f;
-    public override byte ClipSize { get; set; } = 3;
-    
-    [Description("Whether the tranquilizer is effective on SCP-173.")]
-    public bool EffectiveOn173 { get; set; } = false;
-    
-    [Description("The effectiveness of tranquilizer on SCPs in decimal percentage.")]
-    public float ScpChance { get; set; } = 0.5f;
-    
-    [Description("The effectiveness of tranquilizer on humans in decimal percentage.")]
-    public float HumanChance { get; set; } = 0.75f;
+  public override string Name { get; set; } = "Tranquilizer";
+  public override uint Id { get; set; } = 1290;
 
-    public override SpawnProperties? SpawnProperties { get; set; } = new() {
-      Limit = 1,
-      DynamicSpawnPoints = [
-        new DynamicSpawnPoint { Chance = 25, Location = SpawnLocationType.InsideLczCafe },
-        new DynamicSpawnPoint { Chance = 25, Location = SpawnLocationType.InsideLczWc },
-        new DynamicSpawnPoint { Chance = 75, Location = SpawnLocationType.Inside330Chamber }
-      ],
-    };
-  
-    protected override void OnShot(ShotEventArgs ev) {
-      var rand = Random.value;
-      var effective = ev.Player.IsScp ? rand < ScpChance : rand < HumanChance;
-      if ((ev.Player.Role == RoleTypeId.Scp173) || !effective) return;
+  public override string Description { get; set; } =
+    "A gun that temporarily tranquilizes entities; might be unreliable.";
 
-      var lift = Lift.List.First(lift => lift.IsInElevator(ev.Player.Position));
-      ev.Player.Scale = Vector3.zero;
-      ev.Player.EnableEffect(EffectType.Ensnared, byte.MaxValue);
-      ev.Player.EnableEffect(EffectType.Flashed, byte.MaxValue);
-      ev.Player.EnableEffect(EffectType.Deafened, byte.MaxValue);
+  public override float Weight { get; set; } = 1f;
+  public override float Damage { get; set; } = 1f;
+  public override byte ClipSize { get; set; } = 3;
 
-      Timing.CallDelayed(5, () => {
-        ev.Player.DisableEffect(EffectType.Ensnared);
-        ev.Player.DisableEffect(EffectType.Flashed);
-        ev.Player.DisableEffect(EffectType.Deafened);
-        if (lift != null) ev.Player.Teleport(lift.Position + Vector3.up * 1.5f);
-        ev.Player.Scale = Vector3.one;
-      });
-      
-      base.OnShot(ev);
-    }
+  [Description("Whether the tranquilizer is effective on SCP-173.")]
+  public bool EffectiveOn173 { get; set; } = false;
+
+  [Description("The effectiveness of tranquilizer on SCPs in decimal percentage.")]
+  public float ScpChance { get; set; } = 0.5f;
+
+  [Description("The effectiveness of tranquilizer on humans in decimal percentage.")]
+  public float HumanChance { get; set; } = 0.75f;
+
+  public override SpawnProperties? SpawnProperties { get; set; } = new() {
+    Limit = 1,
+    DynamicSpawnPoints = [
+      new DynamicSpawnPoint { Chance = 25, Location = SpawnLocationType.InsideLczCafe },
+      new DynamicSpawnPoint { Chance = 25, Location = SpawnLocationType.InsideLczWc },
+      new DynamicSpawnPoint { Chance = 75, Location = SpawnLocationType.Inside330Chamber }
+    ]
+  };
+
+  protected override void OnShot(ShotEventArgs ev) {
+    var rand = Random.value;
+    var effective = ev.Player.IsScp ? rand < ScpChance : rand < HumanChance;
+    if (ev.Player.Role == RoleTypeId.Scp173 || !effective) return;
+
+    var lift = Lift.List.First(lift => lift.IsInElevator(ev.Player.Position));
+    ev.Player.Scale = Vector3.zero;
+    ev.Player.EnableEffect(EffectType.Ensnared, byte.MaxValue);
+    ev.Player.EnableEffect(EffectType.Flashed, byte.MaxValue);
+    ev.Player.EnableEffect(EffectType.Deafened, byte.MaxValue);
+
+    Timing.CallDelayed(5, () => {
+      ev.Player.DisableEffect(EffectType.Ensnared);
+      ev.Player.DisableEffect(EffectType.Flashed);
+      ev.Player.DisableEffect(EffectType.Deafened);
+      if (lift != null) ev.Player.Teleport(lift.Position + Vector3.up * 1.5f);
+      ev.Player.Scale = Vector3.one;
+    });
+
+    base.OnShot(ev);
+  }
 }
