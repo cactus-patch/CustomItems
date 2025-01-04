8d1e45cfa1ccaae3672d8d15512ef7ca83347e4c
Noobest1001 <99349599+Noobest1001@users.noreply.github.com>
2025-01-04 01:39:31 -0500

    VERY VERY rough implementation of the hat spawning

--- Items/Scp1499.cs
+++ Items/Scp1499.cs
@@ -1,5 +1,28 @@
+using System.ComponentModel;
+using CustomItems.Types;
+using Exiled.API.Enums;
+using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Spawn;
+using Exiled.CustomItems.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerEvents = Exiled.Events.Handlers.Player;
+using MEC;
+
 namespace CustomItems.Items;
 
-public class SCP1499 {
-  
-}
+[CustomItem(ItemType.SCP268)]
+public class SCP1499 : CustomItem {
+  public override string Name { get; set; } = "SCP-1499";
+  public override uint Id { get; set; } = 394;
+  public override string Description { get; set; } = "<i>\"Experience purgatory with a return ticket"</i>";
+  public override float Weight { get; set; } = 1f;
+
+  public override SpawnProperties? SpawnProperties { get; set; } = new() {
+    Limit = 1,
+    DynamicSpawnPoints = [
+      new DynamicSpawnPoint() { Location = SpawnLocationType.InsideLczCafe, Chance = 100 },
+      /* This is temp.
+         Lain ( or Biggie) can you make it so that it can spawn the glass boxes plz :)
+      */
+    ]
+  };
