e1c0e45f32ad9a5aa3e8ede6c1a42dcfa1066cd8
Noobest1001 <99349599+Noobest1001@users.noreply.github.com>
2025-02-09 23:50:40 -0500

    Updated the Coins name and ID

--- Items/Coin.cs
+++ Items/Coin.cs
@@ -12,55 +12,55 @@ namespace CustomItems.Items;
 
 [CustomItem(ItemType.Coin)]
 public class Coin : CustomItem {
-  public override string Name { get; set; } = "Coin";
-  public override uint Id { get; set; } = 393;
-  public override string Description { get; set; } = "<i>\"What's the most you ever lost on a coin toss?\"</i>";
-  public override float Weight { get; set; } = 1f;
+  	public override string Name { get; set; } = "SCP-1289";
+  	public override uint Id { get; set; } = 804;
+  	public override string Description { get; set; } = "<i>\"What's the most you ever lost on a coin toss?\"</i>";
+  	public override float Weight { get; set; } = 1f;
 
-  public override SpawnProperties? SpawnProperties { get; set; } = new() {
-    Limit = 3,
-    RoomSpawnPoints = [
-      new RoomSpawnPoint() { Room = RoomType.LczCafe, Chance = 50 },
-      new RoomSpawnPoint() { Room = RoomType.LczToilets, Chance = 50 },
-      new RoomSpawnPoint() { Room = RoomType.HczArmory, Chance = 50 }
-    ],
-    DynamicSpawnPoints = [
-      new DynamicSpawnPoint() { Location = SpawnLocationType.InsideEscapePrimary, Chance = 50 }
-    ]
-  };
+  	public override SpawnProperties? SpawnProperties { get; set; } = new() {
+		Limit = 3,
+		RoomSpawnPoints = [
+	  		new RoomSpawnPoint() { Room = RoomType.LczCafe, Chance = 50 },
+	  		new RoomSpawnPoint() { Room = RoomType.LczToilets, Chance = 50 },
+	  		new RoomSpawnPoint() { Room = RoomType.HczArmory, Chance = 50 }
+		],
+		DynamicSpawnPoints = [
+	  		new DynamicSpawnPoint() { Location = SpawnLocationType.InsideEscapePrimary, Chance = 50 }
+		]
+  	};
 
-  [Description("Effects to give if coin landed on heads.")]
-  public CoinEffect[] Effects { get; set; } = [
-    new() { Type = EffectType.DamageReduction, Duration = 15, Intensity = 75 },
-    new() { Type = EffectType.RainbowTaste, Duration = 15, Intensity = byte.MaxValue },
-    new() { Type = EffectType.Invigorated, Duration = 15, Intensity = byte.MaxValue },
-    new() { Type = EffectType.MovementBoost, Duration = 15, Intensity = 75 }
-  ];
+  	[Description("Effects to give if coin landed on heads.")]
+	public CoinEffect[] Effects { get; set; } = [
+		new() { Type = EffectType.DamageReduction, Duration = 15, Intensity = 75 },
+		new() { Type = EffectType.RainbowTaste, Duration = 15, Intensity = byte.MaxValue },
+		new() { Type = EffectType.Invigorated, Duration = 15, Intensity = byte.MaxValue },
+		new() { Type = EffectType.MovementBoost, Duration = 15, Intensity = 75 }
+  	];
 
-  protected override void SubscribeEvents() {
-    PlayerEvents.FlippingCoin += OnFlippingCoin;
+  	protected override void SubscribeEvents() {
+		PlayerEvents.FlippingCoin += OnFlippingCoin;
 
-    base.SubscribeEvents();
+		base.SubscribeEvents();
   }
 
   protected override void UnsubscribeEvents() {
-    PlayerEvents.FlippingCoin -= OnFlippingCoin;
+		PlayerEvents.FlippingCoin -= OnFlippingCoin;
 
-    base.UnsubscribeEvents();
+		base.UnsubscribeEvents();
   }
 
-  private void OnFlippingCoin(FlippingCoinEventArgs ev) {
-    if (!Check(ev.Item)) return;
-    Timing.CallDelayed(2f, () => {
-      if (ev.IsTails && !ev.Player.IsDead) {
-        ev.Player.IsGodModeEnabled = false;
-        ev.Player.Explode();
-        ev.Player.Kill("You lost.");
-        return;
-      }
+	private void OnFlippingCoin(FlippingCoinEventArgs ev) {
+		if (!Check(ev.Item)) return;
+		Timing.CallDelayed(2f, () => {
+	  	if (ev.IsTails && !ev.Player.IsDead) {
+			ev.Player.IsGodModeEnabled = false;
+			ev.Player.Explode();
+			ev.Player.Kill("You lost.");
+			return;
+	  }
 
-      ev.Player.ShowHint("You won -- you feel the adrenaline rushing in your veins.");
-      Effects.ForEach((effect) => { ev.Player.EnableEffect(effect.Type, effect.Intensity, effect.Duration, true); });
-    });
+	  ev.Player.ShowHint("You won -- you feel the adrenaline rushing in your veins.");
+	  Effects.ForEach((effect) => { ev.Player.EnableEffect(effect.Type, effect.Intensity, effect.Duration, true); });
+	});
   }
 }
