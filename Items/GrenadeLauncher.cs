172b5a072b3a7f42f8cb75ed9db83a98cd5fae6b
Noobest1001 <99349599+Noobest1001@users.noreply.github.com>
2025-02-09 23:51:06 -0500

    Update GrenadeLauncher.cs

--- Items/GrenadeLauncher.cs
+++ Items/GrenadeLauncher.cs
@@ -1,4 +1,5 @@
-﻿using Exiled.API.Enums;
+using System.Numerics;
+using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.API.Features.Attributes;
 using Exiled.API.Features.Components;
@@ -6,47 +7,52 @@ using Exiled.API.Features.Items;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
 using Exiled.Events.EventArgs.Player;
+using InventorySystem;
+using InventorySystem.Items;
 
 namespace CustomItems.Items;
 
 [CustomItem(ItemType.GunLogicer)]
 public class GrenadeLauncher : CustomWeapon {
-  public override uint Id { get; set; } = 32233;
-  public override string Name { get; set; } = "Grenade Launcher";
-  public override string Description { get; set; } = "Grenade Launcher";
-  public override float Weight { get; set; } = 10f;
-  public override SpawnProperties? SpawnProperties { get; set; } = new () {
-    RoomSpawnPoints = [
-      new RoomSpawnPoint() { Room = RoomType.Surface, Chance = 100 }
-    ]
-  };
-  public override float Damage { get; set; } = 0f;
-  public override byte ClipSize { get; set; } = 1;
+	public override uint Id { get; set; } = 805;
+	public override string Name { get; set; } = "Grenade Launcher";
+	public override string Description { get; set; } = "Grenade Launcher";
+	public override float Weight { get; set; } = 10f;
+	public override SpawnProperties? SpawnProperties { get; set; } = new () { RoomSpawnPoints = [ new RoomSpawnPoint() { Room = RoomType.Surface, Chance = 100 } ] };
+	public override float Damage { get; set; } = 0f;
+	public override byte ClipSize { get; set; } = 1;
 
-  protected override void OnShooting(ShootingEventArgs ev) {
-    var throwable = ev.Player.ThrowGrenade(ProjectileType.FragGrenade);
-    throwable.Projectile.GameObject.AddComponent<CollisionHandler>().Init(ev.Player.GameObject, throwable.Projectile.Base);
-    ev.Player.AddAmmo(AmmoType.Nato762, 1);
-    base.OnShooting(ev);
-  }
+	protected override void OnShooting(ShootingEventArgs ev) {
+		var throwable = ev.Player.ThrowGrenade(ProjectileType.FragGrenade);
+		throwable.Projectile.GameObject.AddComponent<CollisionHandler>().Init(ev.Player.GameObject, throwable.Projectile.Base);
+		ev.Player.AddAmmo(AmmoType.Nato762, 1);
+		ev.Firearm.MagazineAmmo = 0;
+		ev.Firearm.BarrelAmmo = 0;
 
-  protected override void OnReloading(ReloadingWeaponEventArgs ev) {
-    var grenade = ev.Player.Items.First((item) => item.Type == ItemType.GrenadeHE);
-    if (grenade == null) {
-      ev.IsAllowed = false;
-      base.OnReloading(ev);
-      return;
-    }
-    
-    ev.Player.RemoveItem(grenade);
-    
-    base.OnReloading(ev);
-  }
+		base.OnShooting(ev);
+	}
 
-  private static IEnumerator<float> Detonate(Throwable throwable) {
-    for (;;) {
-      var yVelocity = throwable.Projectile.Rigidbody.velocity.y;
-      Log.Info(yVelocity);
-    }
-  }
+	protected override void OnReloading(ReloadingWeaponEventArgs ev) {
+		var grenade = ev.Player.Items.First((item) => item.Type == ItemType.GrenadeHE);
+		bool hasBoom = ev.Player.Inventory.TryGetInventoryItem(ItemType.GrenadeHE, out ItemBase value);
+		if (hasBoom)
+		{
+			ev.IsAllowed = false;
+			ev.Player.ShowHint("You need to find a High Explosive Grenade");
+			base.OnReloading(ev);
+			return;
+		}
+		ev.Player.RemoveItem(grenade);
+		base.OnReloading(ev);
+
+	base.OnReloading(ev);
+	}
+
+	private static IEnumerator<float> Detonate(Throwable throwable) {
+	for (;;) {
+			float comp = -2;
+			var yVelocity = throwable.Projectile.Rigidbody.velocity.y - comp;
+			Log.Info(yVelocity);
+	}
+	}
 }
