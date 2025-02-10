62f5901ae976686193ec7cd834fad66d6c092799
Noobest1001 <99349599+Noobest1001@users.noreply.github.com>
2025-02-10 10:59:40 -0500

    Ported over the shooting logic from the Grenade Launcher
    The Sniper is now a single shot but refunds the ammo instead of eating all the bullets

--- Items/Sniper.cs
+++ Items/Sniper.cs
@@ -11,48 +11,55 @@ namespace CustomItems.Items;
 
 [CustomItem(ItemType.GunE11SR)]
 public class Sniper : CustomWeapon {
-  public override uint Id { get; set; } = 802;
-  public override string Name { get; set; } = "SR-119";
+  	public override uint Id { get; set; } = 802;
+  	public override string Name { get; set; } = "SR-119";
 
-  public override string Description { get; set; } =
-    "A modified E-11 that fires 5.56 at supersonic velocity that deals significantly more damage";
+  	public override string Description { get; set; } = "A modified E-11 that fires 5.56 at supersonic velocity that deals significantly more damage";
 
-  public override float Weight { get; set; } = 8f;
-  public override float Damage { get; set; } = 112f;
-  public override byte ClipSize { get; set; } = 1;
+  	public override float Weight { get; set; } = 8f;
+  	public override float Damage { get; set; } = 112f;
+  	public override byte ClipSize { get; set; } = 1;
 
-  [YamlIgnore]
-  public override AttachmentName[] Attachments { get; set; } = [
-    AttachmentName.LowcapMagAP,
-    AttachmentName.Foregrip,
-    AttachmentName.ScopeSight,
-    AttachmentName.RecoilReducingStock,
-    AttachmentName.RifleBody,
-    AttachmentName.SoundSuppressor
-  ];
+  	[YamlIgnore]
+  	public override AttachmentName[] Attachments { get; set; } = [
+		AttachmentName.LowcapMagAP,
+		AttachmentName.Foregrip,
+		AttachmentName.ScopeSight,
+		AttachmentName.RecoilReducingStock,
+		AttachmentName.RifleBody,
+		AttachmentName.SoundSuppressor
+  	];
 
-  public override SpawnProperties? SpawnProperties { get; set; } = new() {
-    Limit = 0,
-    RoomSpawnPoints = [
-      new RoomSpawnPoint() { Room = RoomType.HczArmory, Chance = 100 }
-    ]
-  };
+  	public override SpawnProperties? SpawnProperties { get; set; } = new() {
+		Limit = 0,
+		RoomSpawnPoints = [
+	  		new RoomSpawnPoint() { Room = RoomType.HczArmory, Chance = 100 }
+		]
+  	};
 
-  protected override void SubscribeEvents() {
-    ItemEvents.ChangingAttachments += OnChangingAttachments;
+  	protected override void SubscribeEvents() {
+		ItemEvents.ChangingAttachments += OnChangingAttachments;
 
-    base.SubscribeEvents();
-  }
+		base.SubscribeEvents();
+  	}
 
-  protected override void UnsubscribeEvents() {
-    ItemEvents.ChangingAttachments -= OnChangingAttachments;
+  	protected override void UnsubscribeEvents() {
+		ItemEvents.ChangingAttachments -= OnChangingAttachments;
 
-    base.UnsubscribeEvents();
-  }
+		base.UnsubscribeEvents();
+  	}
 
-  private void OnChangingAttachments(ChangingAttachmentsEventArgs ev) {
-    if (!Check(ev.Item) || ev.Player.NetId < 2) return;
-    ev.IsAllowed = false;
-    ev.Player.ShowHint("You are not allowed to change the attachment for this weapon.");
-  }
+  	public void OnShooting(ShootingEventArgs ev)
+    {
+   		var initMag = ev.Firearm.MagazineAmmo - 1;
+	 	ev.Firearm.MagazineAmmo = 0;
+   		ev.Firearm.BarrelAmmo = 0;
+		ev.Player.AddAmmo(AmmoType.Nato556, initMag);
+	}
+
+  	private void OnChangingAttachments(ChangingAttachmentsEventArgs ev) {
+		if (!Check(ev.Item) || ev.Player.NetId < 2) return;
+		ev.IsAllowed = false;
+		ev.Player.ShowHint("You are not allowed to change the attachment for this weapon.");
+  	}
 }
