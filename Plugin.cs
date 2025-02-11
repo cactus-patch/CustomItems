f37b4e4c8289f20eccef66afacbce5a40e83b203
Noobest1001 <99349599+Noobest1001@users.noreply.github.com>
2025-02-11 12:32:43 -0500

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -4,23 +4,24 @@ using Exiled.CustomItems.API.Features;
 namespace CustomItems;
 
 public class Plugin : Plugin<Config> {
-  public override string Name => "CactusPatchCustomItems";
-  public override string Author => "furry";
-  public override Version Version => new(9, 0, 0);
-  public override Version RequiredExiledVersion => new(9, 0, 0);
-  public static Plugin? Instance;
+    public override string Prefix => "Cactus Patch";
+    public override string Name => "Custom Items";
+    public override string Author => "Noobest1001";
+    public override Version Version => new(9, 0, 0);
+    public override Version RequiredExiledVersion => new(3, 1, 1);
+    public static Plugin? Instance;
 
-  public override void OnEnabled() {
-    Instance = this;
-    Log.Info("Registering items");
-    CustomItem.RegisterItems(overrideClass: Config);
-    base.OnEnabled();
-  }
+    public override void OnEnabled() {
+      Instance = this;
+      Log.Info("Registering items");
+      CustomItem.RegisterItems(overrideClass: Config);
+      base.OnEnabled();
+    }
 
-  public override void OnDisabled() {
-    Instance = null;
-    Log.Info("Unregistering items");
-    CustomItem.UnregisterItems();
-    base.OnDisabled();
-  }
+    public override void OnDisabled() {
+      Instance = null;
+      Log.Info("Unregistering items");
+      CustomItem.UnregisterItems();
+      base.OnDisabled();
+    }
 }
