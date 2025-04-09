1ac244ca5d101a1d119fda85ec08e9527c524a27
Noobest1001 <obber1532@gmail.com>
2025-04-08 22:59:26 -0400

    started on making commands to assist new players

---
+++ Commands/GuideBook/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSystem;
+
+namespace ExtendedItems.Commands.GuideBook
+{
+    internal sealed class Coinc : ICommand
+    {
+        private Coinc() { }
+
+        public static Coinc Instance { get; } = new();
+        public string Command => "coin";
+        public string[] Aliases => [ "1289" ];
+        public string Description => "Information on SCP-1289 (aka \"Coin\" or \"Colin\") ";
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "<color=white>SCP-1289 (aka \"Coin\" or \"Colin\") is a $0.25 American coin that, when it lands on tails explodes the coin flipper.</color>\n";
+            return true;
+        }
+    }
+}
+
---
+++ Commands/GuideBook/Help.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSystem;
+
+namespace ExtendedItems.Commands.GuideBook
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class Help : ParentCommand
+    {
+        public Help() { LoadGeneratedCommands(); }
+        public static Help Instance { get; } = new();
+        public override string Command => "assistance";
+        public override string[] Aliases => ["h"];
+        public override string Description => "A parent command for helping with ExtendedItems";
+        public override void LoadGeneratedCommands()
+        {
+            RegisterCommand(Coinc.Instance);
+            RegisterCommand(GrenadeLauncher.Instance);
+        }
+
+        protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "Invalid subcommand! Available: Coin, GrenadeLauncher, Plastic, SCP1162, SCP1499, Sniper, Tanq";
+            return false;
+        }
+    }
+}
