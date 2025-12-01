a966db294f05457146e8d75a15d148365d669716
Noobest1001 <obber1532@gmail.com>
2025-12-01 16:19:07 -0500

    Removed the Guns (more working off a theory) and left the non-gun items

--- Config.cs
+++ Config.cs
@@ -10,18 +10,9 @@ namespace ExtendedItems
         public bool Debug { get; set; } = true;
 
         #region Config Settings
-
-        [Description("Weather the Tranq is effective on Tutorials")]
-        public bool EffectiveOnTutorials { get; set; } = false;
-        
-        [Description("Weather the Tranq is still useable when Nuke is on")]
-        public bool NukeTranq { get; set; } = true;
         
         [Description("The Distance from Larry a player can be before they can no longer use SCP-1499")]
         public float LarryDistance { get; set; } = 10f;
-        
-        [Description("Ammount of ammo to remove when Grenade Launcher is fired (Can not be negitive)")]
-        public ushort GrenadeLauncherAmmoUsage { get; set; } = 10;
 
         [Description("The possable hints shown when you Win")]
         public string[] WinHints { get; set; } =
@@ -53,11 +44,7 @@ namespace ExtendedItems
 
         public Coin Coin { get; set; } = new();
         public Scp1499 Scp1499 { get; set; } = new();
-        public GrenadeLauncher GrenadeLauncher { get; set; } = new();
-        public Sniper Sniper { get; set; } = new();
-        public Tranquilizer Tranquilizer { get; set; } = new();
         public Plastic Plastic { get; set; } = new();
-        public AdminAbuse AdminAbuse { get; set; } = new();
 
         #endregion
     }
